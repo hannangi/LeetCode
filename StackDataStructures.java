709e0eacacc831d317f38266c8b2f18769f11b50
hannangi <annangiharikrishna@gmail.com>
2024-10-15 23:30:25 -0400

    Queues

--- StackDataStructures.java
+++ StackDataStructures.java
@@ -1,10 +1,35 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackDataStructures {
     public static void main(String[] args) {
         //stack is a data structure that follows the LIFO (Last In First Out) principle
-        getStack();
+        // getStack();
+        // queue is a data structure that follows the FIFO (First In First Out) principle
+        // enqueue is adding an element to the queue
+        // dequeue is removing an element from the queue
+        queue();
 
+
+    }
+
+    private static void queue() {
+        Queue<String> queue = new LinkedList<String>();
+        System.out.println("Queue: " + queue.isEmpty());
+        queue.offer("A");    // A
+        queue.offer("B");    // A, B
+        queue.offer("C");    // A, B, C
+        // collection methods
+        System.out.println("Queue: " + queue.isEmpty());
+        System.out.println("Queue: " + queue.size());
+        System.out.println("Queue: " + queue.contains("A"));
+        System.out.println("Queue: " + queue);
+        System.out.println("Peek: " + queue.peek());
+        queue.poll();        // B, C
+        System.out.println("Queue after poll: " + queue);
+        // no elemeent
+        
     }
 
     private static void getStack() {
